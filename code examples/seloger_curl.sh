646b1af97ac73b0a23e2dca842243764b276b866
AugustOxy <129506779+AugustOxy@users.noreply.github.com>
2023-12-18 12:44:26 +0200

    Adding code examples/seloger_curl.sh

---
+++ code examples/seloger_curl.sh
@@ -0,0 +1,4 @@
+curl --user user:pass \
+'https://realtime.oxylabs.io/v1/queries' \
+-H "Content-Type: application/json" \
+-d '{"source": "universal", "url": "https://www.seloger.com/immobilier/locations/immo-paris-75/bien-appartement/"}'
